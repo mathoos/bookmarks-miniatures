4003a4643f5570f121d07a2d5fdad603cd9c0dc5
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-03-06 10:02:36 +0100

    Screeshot machine API

--- favoris.js
+++ favoris.js
@@ -1,32 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Récupérer tous les favoris de l'utilisateur
     chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-      const bookmarksList = document.getElementById('bookmarks-list');
-      // Appel à la fonction pour afficher les favoris
-      displayBookmarks(bookmarkTreeNodes, bookmarksList);
+        const bookmarksList = document.querySelector('.bookmarks_container');
+        displayBookmarks(bookmarkTreeNodes, bookmarksList);
     });
-  });
-  
-  function displayBookmarks(bookmarks, parentElement) {
-    bookmarks.forEach(bookmark => {
-      // Si c'est un favori (et non un dossier), on crée un lien
-      if (bookmark.url) {
-        const bookmarkElement = document.createElement('a');
-        bookmarkElement.className = 'bookmark';
-        bookmarkElement.innerHTML = `
-            <a href="${bookmark.url}" target="_blank" class="bookmark_container">
-                <div class="bookmark_container-img"></div>
-                <p class="bookmark_container-title">${bookmark.title}</p>
-            </a>
-        `;
-        parentElement.appendChild(bookmarkElement);
-      }
-  
-      // Si c'est un dossier, on l'affiche également (appel récursif)
-      if (bookmark.children) {
-        displayBookmarks(bookmark.children, parentElement);
-      }
-    });
-  }
-  
+});
 
+function getScreenshot(url) {
+
+    const apiKey = CONFIG.API_KEY;
+    const dimension = "320x240";
+    const device = "desktop";
+    const format = "jpg";
+    const delay = 5000;
+    const encodedUrl = encodeURIComponent(url);
+
+    return `https://api.screenshotmachine.com/?key=${apiKey}&url=${encodedUrl}&device=${device}&dimension=${dimension}&format=${format}&delay=${delay}`;
+}
+
+  
+function displayBookmarks(bookmarks, parentElement) {
+    bookmarks.forEach(bookmark => {
+        if (bookmark.url) {
+            const bookmarkElement = document.createElement('a');
+            bookmarkElement.className = 'bookmark';
+            bookmarkElement.href = bookmark.url;
+            bookmarkElement.setAttribute("target", "_blank");
+
+            const screenshotUrl = getScreenshot(bookmark.url);
+
+            bookmarkElement.innerHTML = `            
+                <img src="${screenshotUrl}" class="bookmark_img"/>
+                <p class="bookmark_title">${bookmark.title}</p>              
+            `;
+
+            parentElement.appendChild(bookmarkElement);
+        }
+
+        // Si c'est un dossier, appel récursif
+        if (bookmark.children) {
+            displayBookmarks(bookmark.children, parentElement);
+        }
+    });
+}
+
+  
